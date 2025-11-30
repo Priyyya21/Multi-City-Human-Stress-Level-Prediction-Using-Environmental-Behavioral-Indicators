cb1ff9f78750b1580cfb7e20c659341f9e0545c4
Priya Sharma <priyaax21@gmail.com>
2025-11-30 18:49:11 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,18 @@
+import sys
+from src.logger import logging
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+
+    return f"Error occurred in script [{file_name}] at line [{line_number}]: {str(error)}"
+
+
+class CustomException(Exception):
+    def __init__(self, error, error_detail: sys):
+        super().__init__(str(error))
+        self.error_message = error_message_detail(error, error_detail)
+
+    def __str__(self):
+        return self.error_message
